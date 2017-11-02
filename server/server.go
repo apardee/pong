13423ccde3dd6953cad2fbcc80562602dac1230b
Anthony Pardee <anthony.pardee@garmin.com>
2017-11-02 14:10:12 -0500

    remove testing code that's been moved over to the test client

--- server/server.go
+++ server/server.go
@@ -199,38 +199,8 @@ func receiveConnection(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Test Logic
-func runWithConnection(conn *websocket.Conn) {
-	for {
-		_, bytes, err := conn.ReadMessage()
-		if err == nil {
-			if bytes != nil {
-				log.Println(string(bytes))
-			}
-		} else {
-			log.Println(err)
-		}
-	}
-}
-
-func runConnection() {
-	conn, resp, err := websocket.DefaultDialer.Dial("ws://localhost:8080", nil)
-	log.Println("Client connected...", resp)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for {
-		// log.Println("Sending a message...")
-		if err := conn.WriteMessage(websocket.TextMessage, []byte("Test...")); err != nil {
-			log.Fatal("Failed to write message...", err)
-			break
-		}
-	}
-}
-
 func main() {
 	mm = newMatchMaker()
-	// go runConnection()
 	go serveMatchRequests(mm)
 	http.HandleFunc("/", receiveConnection)
 	http.ListenAndServe(":8080", nil)
